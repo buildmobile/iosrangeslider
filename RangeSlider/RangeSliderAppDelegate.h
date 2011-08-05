6689c738a9ea69e2e5a488add524f83d69185517
Mal Curtis <mal@sitepoint.com>
2011-08-05 12:56:17 +1200

    Initial Commit

---
+++ RangeSlider/RangeSliderAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  RangeSliderAppDelegate.h
+//  RangeSlider
+//
+//  Created by Mal Curtis on 5/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RangeSliderAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ RangeSlider/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  RangeSlider
+//
+//  Created by Mal Curtis on 5/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
