126715a664979057d51bea978718cc019c3d69a2
Kauserali Hafizji <ali.hafizji@gmail.com>
2012-07-30 09:53:14 +0530

    - Code cleanup - Support for ARC only for the range slider

--- RangeSlider/RangeSlider.h
+++ RangeSlider/RangeSlider.h
@@ -8,29 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RangeSlider : UIControl{
-    float minimumValue;
-    float maximumValue;
-    float minimumRange;
-    float selectedMinimumValue;
-    float selectedMaximumValue;
-    float distanceFromCenter;
-
-    float _padding;
-    
-    BOOL _maxThumbOn;
-    BOOL _minThumbOn;
-    
-    UIImageView * _minThumb;
-    UIImageView * _maxThumb;
-    UIImageView * _track;
-    UIImageView * _trackBackground;
-}
-
-@property(nonatomic) float minimumValue;
-@property(nonatomic) float maximumValue;
-@property(nonatomic) float minimumRange;
-@property(nonatomic) float selectedMinimumValue;
-@property(nonatomic) float selectedMaximumValue;
-
+@interface RangeSlider : UIControl
+@property(nonatomic, readwrite) float minimumValue;
+@property(nonatomic, readwrite) float maximumValue;
+@property(nonatomic, readwrite) float minimumRange;
+@property(nonatomic, readwrite) float selectedMinimumValue;
+@property(nonatomic, readwrite) float selectedMaximumValue;
 @end
